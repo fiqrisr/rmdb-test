0363ff3c4783e1fe1478f7816f507fbc619dbfa2
Fiqri Syah Redha <wibulo27@gmail.com>
2023-10-28 15:35:40 +0700

    fix: hydration error

--- src/components/theme-switcher/theme-switcher.tsx
+++ src/components/theme-switcher/theme-switcher.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Switch } from "@nextui-org/react";
 import { useTheme } from "next-themes";
 
@@ -5,7 +6,16 @@ import { SunIcon } from "@/icons/SunIcon";
 import { MoonIcon } from "@/icons/MoonIcon";
 
 export const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Switch
